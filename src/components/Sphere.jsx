aac0a2189cbd4ae54327564ed0a2ab4a63f0d4df
NordEagle <91448036+NordEagle@users.noreply.github.com>
2023-05-24 21:41:59 +0500

    icons

--- src/components/Sphere.jsx
+++ src/components/Sphere.jsx
@@ -4,17 +4,22 @@ import '../css/Sphere.css';
 
 const Sphere = ({ sign, bg, icon }) => {
   const [isSelected, setIsSelected] = React.useState(false);
+  const interestRef = React.useRef();
+
   let image = require(`../img/spheres/icon${icon}.png`);
   const selectInterest = () => {
     setIsSelected(!isSelected);
     isSelected
-      ? document.querySelector('.interest_sphere').classList.remove('interest_selected')
-      : document.querySelector('.interest_sphere').classList.add('interest_selected');
+      ? interestRef.current.classList.remove('interest_selected')
+      : interestRef.current.classList.add('interest_selected');
   };
 
   return (
     <div className="interest" onClick={selectInterest}>
-      <div className="interest_sphere" style={{ backgroundImage: `url('${bg}')` }}>
+      <div
+        className="interest_sphere"
+        ref={interestRef}
+        style={{ backgroundImage: `url('${bg}')` }}>
         <img src={image} className="interest_icon" alt="interest icon" />
       </div>
       <p className="interest_sign">{sign ? sign : 'TestTestTest'}</p>
